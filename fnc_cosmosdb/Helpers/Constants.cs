585a674fb8ec9f65734890715bf72a42aadc1d24
Pedro Abasto Troncoso <xxpedroatxx@gmail.com>
2020-04-01 00:06:43 -0400

    Helpers

---
+++ fnc_cosmosdb/Helpers/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace fnc_cosmosdb.Helpers
+{
+    public static class Constants
+    {
+        public const string COSMOS_DB_CONNECTION_STRING = "AccountEndpoint=https://syncosmos.documents.azure.com:443/;AccountKey=VcsTQ8IU795lDJzpn1KytrhQaPXVrZbtFSA095OSL51CDzwCHYmx46J2cbdFg3LqxJUNfkzR9JSRDE7NTIbKZw==;";
+        public const string COSMOS_DB_DATABASE_NAME = "synbd";
+        public const string COSMOS_DB_CONTAINER_NAME = "product";
+        public const string COSMOS_DB_ACCOUNT_NAME = "syncosmos";
+
+    }
+}
--- fnc_cosmosdb/ProductInsert.cs
+++ fnc_cosmosdb/ProductInsert.cs
@@ -17,6 +17,7 @@ namespace fnc_cosmosdb
         [FunctionName(nameof(ProductInsert))]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous,  "post", Route = null)] HttpRequest req,
+			
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
