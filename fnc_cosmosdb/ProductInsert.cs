5b4688ffcbd1918529af04d2a48077602a7acf67
SynysterNght <xxpedroatxx@gmail.com>
2020-04-07 20:53:56 -0400

    Completo todo lo ensencial con CosmosDB y anhadiendo exitosamente

--- fnc_cosmosdb/ProductInsert.cs
+++ fnc_cosmosdb/ProductInsert.cs
@@ -11,34 +11,47 @@ namespace fnc_cosmosdb
     using Microsoft.Extensions.Logging;
     using Newtonsoft.Json;
     using fnc_cosmosdb.Models;
+    using fnc_cosmosdb.Helpers;
 
     public class ProductInsert
     {
         [FunctionName(nameof(ProductInsert))]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous,  "post", Route = null)] HttpRequest req,
-			
+			[CosmosDB(
+                        databaseName: Constants.COSMOS_DB_DATABASE_NAME,
+                        collectionName:Constants.COSMOS_DB_CONTAINER_NAME,
+                        ConnectionStringSetting = "StrCosmos"    
+                        )]
+            IAsyncCollector<object> products,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            var data = JsonConvert.DeserializeObject<Product>(requestBody);
-
-            var product = new Product
+            IActionResult returnValue = null;
+            try
             {
-                ProductId = data.ProductId,
-                Provider=data.Provider,
-                Name=data.Name,
-                Price=data.Price
                 
-            };
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                var data = JsonConvert.DeserializeObject<Product>(requestBody);
+                var product = new Product
+                {
+                    ProductId = data.ProductId,
+                    Provider = data.Provider,
+                    Name = data.Name,
+                    Price = data.Price
 
-            string responseMessage = product.Name;
-
-            return new OkObjectResult(responseMessage);
+                };
+                ///Para insertarlo al CosmosDB
+                await products.AddAsync(product);
+                ///////////////
+                log.LogInformation($"Item Inserted {product.Name}");
+                
+                returnValue = new OkObjectResult(product);
+            }
+            catch (Exception ex) {
+                log.LogError($"Could not insert product. Exception: {ex.Message}");
+                returnValue = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+            return returnValue;
         }
     }
 }
